afb159a8d600d4de4c0375e223dc378149823142
AmirTabaee <sir.amir.71@gmail.com>
2022-03-18 23:52:15 +0330

    project react initialized

---
+++ src/container/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../assets/bootstrap.min.css";
+
+const App = () => {
+   return (
+      <>
+         <div>hello world</div>
+         <button className="btn btn-danger">click me</button>
+      </>
+   );
+};
+
+export default App;
